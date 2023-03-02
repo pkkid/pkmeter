a49bd6922651eb0b5dea922c5799d2ccb0e34594
Michael Shepanski <michael.shepanski@gmail.com>
2023-03-02 17:12:11 -0500

    Cleanup Dropshadow logic

--- pkm/qtemplate.py
+++ pkm/qtemplate.py
@@ -290,19 +290,21 @@ class DropShadow:
     """ Applies a dropshadow to the parent element. """
     
     def __init__(self, qtmpl, elem, parent, context, *args):
-        # Args are x, y, blur, opacity(0-255)
-        # Color hardcoded to black
+        # Read the args (x, y, blur, opacity{0-255})
         x = args[0] if len(args) >= 1 else 0
         y = args[1] if len(args) >= 2 else 0
         blur = args[2] if len(args) >= 3 else 0
         opacity = args[3] if len(args) >= 4 else 255
+        # Create the shadow effect and apply it to parent
+        # We save the effect object to parent so it's not garbage collected
         shadow = QtWidgets.QGraphicsDropShadowEffect()
         shadow.setOffset(x, y)
         shadow.setBlurRadius(blur)
         shadow.setColor(QtGui.QColor(0, 0, 0, opacity))
-        log.info('APPLYING SHADOW')
         parent.setGraphicsEffect(shadow)
-        qtmpl._dropshadow = shadow
+        parent._dropshadow = shadow
+        # Set grandparent's margins to allow space for dropshadow
+        parent.parent().setContentsMargins(*[max(x,y)+blur]*4)
         
 
 class Repeater:
